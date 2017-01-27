1d06caadb708f6fc766b50a7de89aa26c3571d68
craig <cbrookes@redhat.com>
2017-01-27 15:43:15 +0000

    add custom authenticator for outbound requests to a http proxy

---
+++ push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/configuration/ProxyConfiguration.java
@@ -0,0 +1,32 @@
+package org.jboss.aerogear.unifiedpush.message.configuration;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
+@Startup
+@Singleton
+/**
+ * responsible for handling authentication challenges from outbound HTTP_PROXY
+ */
+public class ProxyConfiguration {
+
+  @PostConstruct
+  public void setupAuth(){
+    Authenticator.setDefault(new Authenticator() {
+
+      @Override protected PasswordAuthentication getPasswordAuthentication() {
+        String proxyHost = System.getenv("HTTP_PROXY_HOST");
+        String proxyUser = System.getenv("HTTP_PROXY_USER");
+        String proxyPass = System.getenv("HTTP_PROXY_PASS");
+        String hostAskingForAuth = this.getRequestingHost();
+        if(hostAskingForAuth.equals(proxyHost)){
+          return new PasswordAuthentication(proxyUser,proxyPass.toCharArray());
+        }
+        return super.getPasswordAuthentication();
+      }
+    });
+  }
+}
